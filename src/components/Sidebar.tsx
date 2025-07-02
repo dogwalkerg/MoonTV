9c832028c6524691e4b86f012a0dc48663c5d765
bbrplus <36788116+dogwalkerg@users.noreply.github.com>
2025-07-02 18:36:02 +0800

    Update Sidebar.tsx

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -170,8 +170,8 @@ const Sidebar = ({ onToggle, activePath = '/' }: SidebarProps) => {
   if (siteName !== 'MoonTV') {
     menuItems.push({
       icon: Github,
-      label: 'MoonTV',
-      href: 'https://github.com/senshinya/MoonTV',
+      label: '交流群',
+      href: 'https://lt.916919.xyz',
     });
   }
 
